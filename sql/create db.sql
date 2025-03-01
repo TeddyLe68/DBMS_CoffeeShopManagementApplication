f9d3607706f648ed2fe2647dfe7a96d69eaa6404
TeddyLe68 <teddyle2003@gmail.com>
2025-03-01 12:16:38 +0700

    create/insert data

---
+++ sql/create db.sql
@@ -0,0 +1,134 @@
+﻿-- Create CoffeeShopManagement database
+use master;
+GO
+IF DB_ID('CoffeeShopManagement') IS NOT NULL
+DROP DATABASE CoffeeShopManagement;
+
+GO
+CREATE DATABASE CoffeeShopManagement;
+GO
+USE CoffeeShopManagement;
+
+-- Create tables with uniqueidentifier (UUID) primary keys
+GO
+CREATE TABLE Employee (
+    employeeId UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+    fullName NVARCHAR(100),
+    phoneNumber VARCHAR(15),
+    address NVARCHAR(255),
+    email VARCHAR(255) UNIQUE,
+    isWorking BIT,
+    joinedAt DATE,
+    updatedAt DATETIME,
+    isDeleted BIT DEFAULT 0,
+);
+
+GO
+CREATE TABLE Product (
+    productId UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+    productName NVARCHAR(100),
+    productSize NVARCHAR(50),
+    productPrice DECIMAL(10, 2) CHECK (productPrice >= 0.00),
+    createdAt DATETIME,
+    updatedAt DATETIME,
+    isDeleted BIT DEFAULT 0
+);
+
+GO
+CREATE TABLE Customer (
+    customerId UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+    customerName NVARCHAR(100),
+    phoneNumber VARCHAR(15),
+    rewardPoint DECIMAL(10, 2) DEFAULT 0,
+    createdAt DATETIME,
+    updatedAt DATETIME,
+    isDeleted BIT DEFAULT 0,
+);
+
+GO
+CREATE TABLE Ingredient (
+    ingredientId UNIQUEIDENTIFIER DEFAULT NEWID(),
+    ingredientName NVARCHAR(100),
+    manufacturerName NVARCHAR(50),
+    updatedAt DATETIME,
+    isDeleted BIT DEFAULT 0,
+	CONSTRAINT PK_Ingredient PRIMARY KEY (ingredientId)
+);
+
+GO
+CREATE TABLE Inventory (
+    inventoryId UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+    name NVARCHAR(255),
+	isDeleted BIT DEFAULT 0
+);
+
+GO
+CREATE TABLE Account (
+    accountId UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+	employeeId UNIQUEIDENTIFIER REFERENCES Employee(employeeId),
+    username VARCHAR(255) UNIQUE,
+    password VARCHAR(255),
+    role VARCHAR(20),
+    createdAt DATETIME,
+    updatedAt DATETIME,
+    isDeleted BIT DEFAULT 0
+);
+
+
+
+GO
+CREATE TABLE OrderBill (
+    billId UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+	rewardPointsUsed DECIMAL(10, 2) CHECK (rewardPointsUsed >= 0.00),
+    initialBill DECIMAL(10, 2) CHECK (initialBill >= 0.00) DEFAULT 0.00,
+    finalBill DECIMAL(10, 2) CHECK (finalBill >= 0.00) DEFAULT 0.00,
+    createdAt DATETIME,
+    isDeleted BIT DEFAULT 0,
+    employeeId UNIQUEIDENTIFIER REFERENCES Employee(employeeId),
+    customerId UNIQUEIDENTIFIER REFERENCES Customer(customerId)
+);
+
+GO
+CREATE TABLE RestockBill (
+    restockBillId UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+    date DATE,
+	supplierName NVARCHAR(50),
+    totalBill DECIMAL(10, 2) CHECK (totalBill >= 0.00) DEFAULT 0.00
+);
+
+GO
+CREATE TABLE OrderBillDetails (
+    billId UNIQUEIDENTIFIER REFERENCES OrderBill(billId),
+    productId UNIQUEIDENTIFIER REFERENCES Product(productId),
+    quantity INT CHECK (quantity >= 1),
+    PRIMARY KEY (billId, productId)
+);
+
+GO
+CREATE TABLE InventoryCheck (
+	checkId UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+    employeeId UNIQUEIDENTIFIER REFERENCES Employee(employeeId),
+    inventoryId UNIQUEIDENTIFIER REFERENCES Inventory(inventoryId),
+    checkDate DATE,
+);
+
+GO
+CREATE TABLE InventoryCheckDetails (
+	checkId UNIQUEIDENTIFIER REFERENCES InventoryCheck(checkId),
+	ingredientId UNIQUEIDENTIFIER REFERENCES Ingredient(ingredientId),
+    quantity INT,
+    PRIMARY KEY (checkId, ingredientId)
+);
+
+GO
+CREATE TABLE RestockBillDetails (
+    ingredientId UNIQUEIDENTIFIER REFERENCES Ingredient(ingredientId),
+	restockBillId UNIQUEIDENTIFIER REFERENCES RestockBill(restockBillId),
+	PRIMARY KEY (ingredientId, restockBillId),
+    quantity INT CHECK (quantity >= 1),
+    price DECIMAL(10, 2),
+);
+
+GO
+ALTER TABLE Product
+ADD CONSTRAINT productSizeConstraint CHECK (productSize = N'Lớn' or productSize = N'Nhỏ' or productSize = N'Vừa' );
---
+++ sql/insertData.sql
@@ -0,0 +1,66 @@
+﻿use CoffeeShopManagement
+
+GO
+INSERT INTO Employee (fullName, phoneNumber, address, email, isWorking, joinedAt, updatedAt)
+VALUES
+	(N'Tất Thắng', '0933344455', N'2 Võ Văn Ngân, TP.HCM', 'thang@gmail.com', 1, CONVERT(DATE, '11/02/2024', 103), GETDATE());
+GO
+INSERT INTO Employee (fullName, phoneNumber, address, email, isWorking, joinedAt, updatedAt)
+VALUES
+	(N'Thành Hiếu', '0987654321', N'1 Võ Văn Ngân, TP.HCM', 'hieu@gmail.com', 1, CONVERT(DATE, GETDATE(), 103), GETDATE());
+GO
+INSERT INTO Employee (fullName, phoneNumber, address, email, isWorking, joinedAt, updatedAt)
+VALUES
+	(N'Phi Tường', '0988877766', N'3 Võ Văn Ngân, TP.HCM', 'tuong@gmail.com', 0, CONVERT(DATE, '30/11/2023', 103), GETDATE());
+GO
+INSERT INTO Employee (fullName, phoneNumber, address, email, isWorking, joinedAt, updatedAt)
+VALUES
+	(N'Minh Đức', '0933355577', N'4 Võ Văn Ngân, TP.HCM', 'duc@gmail.com', 0, CONVERT(DATE, '12/09/2023', 103), GETDATE());
+
+GO
+INSERT INTO Customer (customerName, phoneNumber, createdAt, updatedAt)
+VALUES 
+    (N'Trúc Nga', '123456789', GETDATE(), GETDATE()),
+    (N'Thanh Tuấn', '987654321', GETDATE(), GETDATE()),
+	(N'Ngọc Mai', '089876656', GETDATE(), GETDATE()),
+    (N'Cao Kha', '09083636', GETDATE(), GETDATE()),
+	(N'Trúc Nga', '098878767', GETDATE(), GETDATE()),
+    (N'Điệp Khúc', '096126322', GETDATE(), GETDATE()),
+    (N'Hiệp', '555123456', GETDATE(), GETDATE());
+
+GO
+INSERT INTO Product (productName, productSize, productPrice, createdAt, updatedAt)
+VALUES 
+    (N'Cà phê đen', N'Nhỏ', 20.000, GETDATE(), GETDATE()),
+    (N'Cà phê đen', N'Vừa', 25.000, GETDATE(), GETDATE()),
+    (N'Cà phê sữa', N'Nhỏ', 25.000, GETDATE(), GETDATE()),
+    (N'Cà phê sữa', N'Vừa', 30.000, GETDATE(), GETDATE()),
+    (N'Bạc Sĩu', N'Nhỏ', 25.000, GETDATE(), GETDATE()),
+    (N'Bạc Sĩu', N'Vừa', 30.000, GETDATE(), GETDATE()),
+	(N'Sinh tố Bơ', N'Vừa', 35.000, GETDATE(), GETDATE()),
+    (N'Sinh tố Bơ', N'Lớn', 40.000, GETDATE(), GETDATE()),
+	(N'Sinh tố Dâu', N'Vừa', 35.000, GETDATE(), GETDATE()),
+    (N'Sinh tố Dâu', N'Lớn', 40.000, GETDATE(), GETDATE());
+-- Chạy từng câu và sửa employeeeId, customerId, billId lại
+
+
+GO
+INSERT INTO OrderBill (rewardPointsUsed, createdAt, employeeId, customerId)
+VALUES (25.000 , GETDATE(), 'CE8198DA-05EC-46A9-A3F8-E5E64ECE8864', '1E155C77-BD6E-4B65-B030-0ACE23F14887'),
+		(0, GETDATE(), 'CE8198DA-05EC-46A9-A3F8-E5E64ECE8864', '1E155C77-BD6E-4B65-B030-0ACE23F14887'),
+		(0, GETDATE(), 'CE8198DA-05EC-46A9-A3F8-E5E64ECE8864', '1E155C77-BD6E-4B65-B030-0ACE23F14887');
+GO
+INSERT INTO OrderBillDetails (billId, productId, quantity)
+VALUES ('6FDD5347-7377-4A0E-BD3A-12D1DC009B4D', '1A19CC39-53BC-4006-AE8C-294E43D2BD12', 2),
+		('6FDD5347-7377-4A0E-BD3A-12D1DC009B4D', '5CC62243-38A7-43C6-BF20-67293635503D', 2);
+
+GO
+INSERT INTO OrderBillDetails (billId, productId, quantity)
+VALUES ('9847D318-FDD5-4772-8133-AD688C4A6D87', '8CF7DCE7-DE32-4619-9642-71F1C47FA42F', 2),
+		('9847D318-FDD5-4772-8133-AD688C4A6D87', 'DF8FA6FF-741D-4006-BD6D-7EFCC9231BCB', 1),
+		('9847D318-FDD5-4772-8133-AD688C4A6D87', '19F4909C-95C6-45F5-9239-84D60B9BF570', 3),
+		('9847D318-FDD5-4772-8133-AD688C4A6D87', 'E6B176FE-622F-431F-9512-9C3092A43DC6', 2);
+
+GO
+INSERT INTO OrderBillDetails (billid, productId, quantity)
+VALUES ('FACAF814-15AC-460E-957B-EEDAAF11C3DC', 'DEC1F5A1-DBE6-467A-9C28-A6B8CAA7597F', 3);
