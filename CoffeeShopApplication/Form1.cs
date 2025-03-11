becce6d0726015eb3857a228a96124f3a4d754b4
TeddyLe68 <teddyle2003@gmail.com>
2025-03-11 14:04:01 +0700

    connect database

--- CoffeeShopApplication/Form1.cs
+++ CoffeeShopApplication/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CoffeeShopApplication.DB;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,24 @@ namespace CoffeeShopApplication
 {
     public partial class Form1: Form
     {
+        ConnectionDB db = ConnectionDB.getInstance();
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (db.TestConnection())
+            {
+                MessageBox.Show("Kết nối thành công!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Kết nối thất bại!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
